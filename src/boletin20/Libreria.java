e0b26e7449f49ea8f23927b881c1f870a5bfc0a0
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-21 09:09:22 +0100

    Metodo para buscar libros por titulo

--- src/boletin20/Libreria.java
+++ src/boletin20/Libreria.java
@@ -1,6 +1,7 @@
 package boletin20;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import javax.swing.JOptionPane;
 
 /**
@@ -19,6 +20,22 @@ public class Libreria {
         int uni=Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de unidades del libro"));
         nombre.add(new Libro(titulo,aut,pre,uni));
         
+    }
+    /**
+     * Es un metodo estatico que recive un ArrayList y no devuelve nada.
+     * Este metodo se encarga de comparar en el Arraylist el titulo introducido con algun titulo existente
+     * @param nombre 
+     */
+    public static void consultar(ArrayList<Libro>nombre){
+        String titulo=JOptionPane.showInputDialog("Introduce el nombre del Libro");
+        for(int i=0;i<nombre.size();i++){
+            if(titulo==nombre.get(i).getNombre()){
+                System.out.println("Su precio es de :"+nombre.get(i).getPrecio());
+            }else {
+                System.out.println("El libro no está en la libreria");
+            }
+            
+        }
     }
 
 }
