1499459aeacd285c56e3901836abbfd3c8cd871b
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-22 14:01:31 +0100

    Proyecto acabado

--- src/boletin20/Boletin20.java
+++ src/boletin20/Boletin20.java
@@ -12,29 +12,37 @@ public class Boletin20 {
         int m;
         Libreria a=new Libreria();
         a.archivoAArray();
-//       a.comprobarFichero();
         do {
             m=Integer.parseInt(JOptionPane.showInputDialog("*******Menu******\n1 Engadir libro a la Libreria\n2 Consultar datos por Titulo\n3 Visualizar todos los datos de la Libreria\n4 Borrar libros con 0 unidades \n5 Modificar el Precio de un Libro\n0 Salir"));
+
             switch (m) {
 
                 case 1:
-                    
+
                     a.engadir();
                     a.escribirFichero();
-                    
+                    System.out.println("\n\n");
+
                     break;
                 case 2:;
+                   
                     a.consultar();
+                    System.out.println("\n\n");
                     break;
 
                 case 3:
                     a.leerLibreria();
+                    System.out.println("\n\n");
                     break;
-                case 4:a.borrado0();
+                case 4:
+                    a.borrado0();
+                    System.out.println("\n\n");
                     break;
-                    
-                case 5:a.modificarPrecio();
 
+                case 5:
+                    
+                    a.modificarPrecio();
+                    System.out.println("\n\n");
                     break;
 
             }
